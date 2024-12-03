e02fcb085496c317200f0bd22390b0f5bd897018
vimthasekhar <vimthasekhar@gmail.com>
2024-12-03 20:11:04 +0530

    My_third_commit

--- Example1.py
+++ Example1.py
@@ -1,5 +1,16 @@
 a,b=10,5
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+
+def add():
+  print(a+b)
+def sub():
+  print(a-b)
+def mul():
+  print(a*b)
+def div():
+  print(a/b)
+
+if __name__=="__main__":
+  add()
+  sub()
+  mul()
+  div()
